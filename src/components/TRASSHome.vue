836276b53e6ddbeed82fb08280ca191e49ba630c
Going9 <ickdor123@gmail.com>
2025-03-14 11:11:18 +0900

    Init: project

---
+++ src/components/TRASSHome.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="greetings">
+    <h1>TRASS 자동화 시스템</h1>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Components
+import App from './App.vue'
+import router from './router'
+
+// Vuetify 인스턴스 생성
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+const app = createApp(App)
+
+// 플러그인 추가
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
